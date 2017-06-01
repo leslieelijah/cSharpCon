4dce1a9421807d69cee3fa084e893421d848e405
Leslie Mahasha <LMahasha@jhb.dvt.co.za>
2017-06-01 12:29:55 +0200

    Update the data

--- CSharpConsole/Program.cs
+++ CSharpConsole/Program.cs
@@ -10,7 +10,7 @@ namespace CSharpConsole
     {
         public static void Main(string[] args)
         {
-           setter();
+           //setter();
         }
 
         public static void setter()
